8e351fb93d527bdd53c63c4ab079b176fd6d4c35
unknown <michaelangelo4489@gmail.com>
2025-02-14 15:22:47 +0700

    computer ui

--- src/components/Computer.tsx
+++ src/components/Computer.tsx
@@ -6,8 +6,13 @@ interface ComputerProps {
 }
 
 export default function Computer({ ...computerProp } : ComputerProps) {
+  
+  function handleAction(ip: string) {
+    console.log('PC on IP: ', ip, ' is controlled');
+  }
+  
   return (
-    <div className="px-4 py-2 bg-accent rounded-xl cursor-pointer hover:opacity-90">
+    <div onClick={() => { handleAction(computerProp.ip) }} className="px-4 py-2 bg-accent rounded-xl cursor-pointer hover:opacity-90 active:opacity-80">
       <div className="flex flex-col gap-3">
         <div className="font-bold text-2xl">PC {computerProp.order}</div>
         <div className="font-medium text-xl">IP: {computerProp.ip}</div>
