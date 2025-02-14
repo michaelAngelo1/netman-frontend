5fa48c91eed096cd14331a16a0566b396f5bc066
unknown <michaelangelo4489@gmail.com>
2025-02-14 15:10:13 +0700

    ui computer

--- src/components/Computer.tsx
+++ src/components/Computer.tsx
@@ -9,9 +9,9 @@ export default function Computer({ ...computerProp } : ComputerProps) {
   return (
     <div className="px-4 py-2 bg-accent rounded-xl cursor-pointer hover:opacity-90">
       <div className="flex flex-col gap-3">
-        <div>PC {computerProp.order}</div>
-        <div>IP: {computerProp.ip}</div>
-        <div>MAC: {computerProp.mac}</div>
+        <div className="font-bold text-2xl">PC {computerProp.order}</div>
+        <div className="font-medium text-xl">IP: {computerProp.ip}</div>
+        <div className="font-medium text-xl">MAC: {computerProp.mac}</div>
       </div>
     </div>
   )
