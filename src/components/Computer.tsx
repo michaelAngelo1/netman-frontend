492c9c9c7826407d9aa90035809c907f3b7f12d9
unknown <michaelangelo4489@gmail.com>
2025-03-06 18:52:36 +0700

    add checkbox computer

--- src/components/Computer.tsx
+++ src/components/Computer.tsx
@@ -1,3 +1,5 @@
+
+
 interface ComputerProps {
   order: number;
   mac: string;
@@ -6,15 +8,18 @@ interface ComputerProps {
 }
 
 export default function Computer({ ...computerProp } : ComputerProps) {
-  
-  function handleAction(ip: string) {
-    console.log('PC on IP: ', ip, ' is controlled');
+
+  function handleAction({ ...computerProp }, checked: boolean) {
+    console.log('PC on IP: ', computerProp.ip, ' is ', checked.toString());
   }
   
   return (
-    <div onClick={() => { handleAction(computerProp.ip) }} className="px-4 py-2 bg-accent rounded-xl cursor-pointer hover:opacity-90 active:opacity-80">
+    <div className="px-4 py-2 bg-accent rounded-xl cursor-pointer hover:opacity-90 active:opacity-80">
       <div className="flex flex-col gap-3">
-        <div className="font-bold text-2xl">PC {computerProp.order}</div>
+        <div className="flex flex-row gap-3">
+          <div className="font-bold text-2xl">PC {computerProp.order}</div>
+          <input onChange={(e) => { handleAction({ ...computerProp }, e.target.checked) }} type="checkbox" className="checkbox checkbox-xl checkbox-primary border-2 border-blue-50"/>
+        </div>
         <div className="font-medium text-xl">IP: {computerProp.ip}</div>
         <div className="font-medium text-xl">MAC: {computerProp.mac}</div>
       </div>
--- src/components/ModalAction.tsx
+++ src/components/ModalAction.tsx
@@ -7,14 +7,30 @@ interface ModalActionProps {
 
 export default function ModalAction({ open, action, setOpen } : ModalActionProps) {
   if (!open) {
-    return null; // Return null if 'open' is false, preventing the modal from rendering.
+    return null; 
   }
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-transparent">
-      <div className="bg-white p-6 rounded-lg text-black">
-        <p>Action: {action}</p>
-        <p>Opened: {open.toString()}</p>
-        <div className="text-black text-xl cursor-pointer underline" onClick={() => setOpen(false)}>Close</div>
+      <div className="bg-white p-6 w-1/4 rounded-lg text-black">
+        {
+          action == "control" ?
+            <div className="flex flex-col gap-3">
+              <div className="text-2xl font-bold text-center">List of controls</div>
+              <div className="flex flex-col gap-3">
+                <div className="text-lg font-medium p-3 border-2 border-accent rounded-xl hover:bg-accent hover:text-slate-50">Turn on PC</div>
+                <div className="text-lg font-medium p-3 border-2 border-accent rounded-xl hover:bg-accent hover:text-slate-50">Shut down PC</div>
+                <div className="text-lg font-medium p-3 border-2 border-accent rounded-xl hover:bg-accent hover:text-slate-50">Start Chrome</div>
+                <div className="text-lg font-medium p-3 border-2 border-accent rounded-xl hover:bg-accent hover:text-slate-50">Open File Explorer</div>
+              </div>
+            </div>
+          :
+            <div className="">
+
+            </div>
+        }
+        <div className="flex justify-center mt-4">
+          <div className="btn btn-neutral p-4 rounded-xl text-xl cursor-pointer" onClick={() => setOpen(false)}>Close</div>
+        </div>
       </div>
     </div>
   );
