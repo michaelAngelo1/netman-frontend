feb0fe7b33325a0438dab458af2aa371ddffa982
joheee <johevinblesstowi07@gmail.com>
2025-05-24 12:25:26 +0700

    testing: rebuild

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,27 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const backendApiUrl = import.meta.env.VITE_BACKEND_URL;
+console.log("backendApiUrl: ", backendApiUrl);
 
 export const roomInstance = axios.create({
   baseURL: `${backendApiUrl}/room`,
   headers: {
-    accept: '*/*',
-    'Content-Type': 'application/json'
+    accept: "*/*",
+    "Content-Type": "application/json",
   },
 });
 
 export const commandInstance = axios.create({
   baseURL: `${backendApiUrl}/command`,
   headers: {
-    accept: '*/*',
-    'Content-Type': 'application/json'
+    accept: "*/*",
+    "Content-Type": "application/json",
   },
 });
 
-export const commandByIdInstance = (id: string) => axios.create({
-  baseURL: `${backendApiUrl}/command/${id}`,
-  headers: {
-    accept: '*/*',
-    'Content-Type': 'application/json'
-  },
-});
+export const commandByIdInstance = (id: string) =>
+  axios.create({
+    baseURL: `${backendApiUrl}/command/${id}`,
+    headers: {
+      accept: "*/*",
+      "Content-Type": "application/json",
+    },
+  });
